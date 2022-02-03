4cffb9723a28305d4898d0cb5f6466fec97b2028
vatdut8994 <82714982+vatdut8994@users.noreply.github.com>
2022-02-03 17:58:10 -0500

    Update webbrowser.py

--- webbrowser.py
+++ webbrowser.py
@@ -5,25 +5,13 @@ from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtPrintSupport import *
 import sys
 
-# creating main window class
 class MainWindow(QMainWindow):
-
-	# constructor
-	def __init__(self, *args, **kwargs):
-		super(MainWindow, self).__init__(*args, **kwargs)
-
-		self.browser = QWebEngineView()
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.browser = QWebEngineView()
         self.browser
 
-
-# creating a pyQt5 application
 app = QApplication(sys.argv)
-
-# setting name to the application
 app.setApplicationName("Geek Browser")
-
-# creating a main window object
 window = MainWindow()
-
-# loop
 app.exec_()
